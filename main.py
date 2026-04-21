d968c59064bfc243d8b152927cd57e281b20fd70
djayco <d.coret@icloud.com>
2026-04-21 13:39:34 +0200

    this time really added the venv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from transformers import AutoModelForPreTraining, AutoProcessor
+from transformers import AutoFeatureExtractor
 
-processor = AutoProcessor.from_pretrained("facebook/wav2vec2-xls-r-1b")
-model = AutoModelForPreTraining.from_pretrained("facebook/wav2vec2-xls-r-1b")
+processor = AutoFeatureExtractor.from_pretrained("facebook/wav2vec2-xls-r-1b")
 
 print(dir(model))
